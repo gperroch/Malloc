299564ba46ce44fba9fc5b338ed946d87cbade0b
Guillaume PERROCHEAU <perrocheau.guillaume@gmail.com>
2017-09-19 11:33:18 +0200

    Mise a la norme de malloc.h. Bonus toujours a faire.

--- INCLUDES/malloc.h
+++ INCLUDES/malloc.h
@@ -6,7 +6,7 @@
 /*   By: gperroch <gperroch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/14 15:05:50 by gperroch          #+#    #+#             */
-/*   Updated: 2017/09/19 11:09:48 by gperroch         ###   ########.fr       */
+/*   Updated: 2017/09/19 11:32:38 by gperroch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,24 @@
 # include <unistd.h>
 # include <sys/mman.h>
 # include "libftprintf.h"
-# define TINY 128 //64
+# define TINY 128
 # define SMALL 1024
-# define AREA_TINY (((TINY + sizeof(t_metadata)) * 106) + sizeof(t_metadata) + 64)
-# define AREA_SMALL (((SMALL + sizeof(t_metadata)) * 101) + sizeof(t_metadata) + 640)
-# define align4(x) ((((x - 1) >> 2) << 2) + 4)
+# define SIZE_METADATA sizeof(t_metadata)
+# define AREA_TINY (((TINY + SIZE_METADATA) * 106) + SIZE_METADATA + 64)
+# define AREA_SMALL (((SMALL + SIZE_METADATA) * 101) + SIZE_METADATA + 640)
 # define MAGIC_NUMBER_BLOC 0x810C
 # define MAGIC_NUMBER_AREA 0x811C
 
 /*
- * size_total - BLOC : correspond a la taille effective du bloc. AREA : correspond a la taille totale de la zone.
- * prev_area  - BLOC : adresse de la zone correspondante. AREA : adresse de la zone precedente.
- * size_data  - BLOC : correspond a la taille maximum du bloc. AREA : comme pour les BLOC.
- *
- */
+** size_total - 	BLOC : correspond a la taille effective du bloc.
+**					AREA : correspond a la taille totale de la zone.
+** prev_area  - 	BLOC : adresse de la zone correspondante.
+**					AREA : adresse de la zone precedente.
+** size_data  - 	BLOC : correspond a la taille maximum du bloc.
+**					AREA : comme pour les BLOC.
+*/
 
-extern void *g_start;
-
+extern void				*g_start;
 typedef struct			s_metadata
 {
 	size_t				size_total;
@@ -45,29 +46,41 @@ typedef struct			s_metadata
 	size_t				size_data;
 	char				padding[24];
 }						t_metadata;
-void			*malloc(size_t size);
-void			*realloc(void *ptr, size_t size);
-void			dump_mem(void *ptr, int len, int col, char *name);
-void			show_alloc_mem(void);
-void			free(void *ptr);
-int				ft_find_area(void *start, t_metadata **area, size_t size);
-int				ft_find_bloc(t_metadata *area, t_metadata **bloc, size_t size);
-int				ft_update_metadata(t_metadata *bloc, size_t size);
-int				ft_size_available(t_metadata *area, t_metadata *bloc, size_t size);
-int				ft_new_area(t_metadata *start, t_metadata **area, size_t size);
-int				ft_add_next_metadata(t_metadata *bloc, t_metadata *area);
-int				ft_find_bloc_area(t_metadata *area, t_metadata **target, size_t size, int bloc);
-static void 	ft_area_free(t_metadata *area);
-static void 			ft_print_area(char *str, t_metadata *bloc);
+void					*malloc(size_t size);
+void					*realloc(void *ptr, size_t size);
+void					dump_mem(void *ptr, int len, int col, char *name);
+void					show_alloc_mem(void);
+void					free(void *ptr);
+int						ft_find_area(void *start, t_metadata **area,
+	size_t size);
+int						ft_find_bloc(t_metadata *area, t_metadata **bloc,
+	size_t size);
+int						ft_update_metadata(t_metadata *bloc, size_t size);
+int						ft_size_available(t_metadata *area, t_metadata *bloc,
+	size_t size);
+int						ft_new_area(t_metadata *start, t_metadata **area,
+	size_t size);
+int						ft_add_next_metadata(t_metadata *bloc,
+	t_metadata *area);
+int						ft_find_bloc_area(t_metadata *area, t_metadata **target,
+	size_t size, int bloc);
+static void				ft_area_free(t_metadata *area);
+static void				ft_print_area(char *str, t_metadata *bloc);
 static uintmax_t		ft_address(void *ptr);
-static void			ft_display_addr(void *ptr);
-static void 			ft_print_bloc(t_metadata *bloc);
-static	void	ft_init_first_bloc(t_metadata *first_bloc, t_metadata *new_area);
-int				ft_new_area(t_metadata *start, t_metadata **area, size_t size);
-static void			ft_init_new_area(t_metadata *new_area, size_t size_total, size_t size);
-static void			ft_add_to_last_area(t_metadata *start, t_metadata *new_area);
-static int		ft_add_extra_bloc(void *ptr, size_t size, size_t size_total, t_metadata *bloc);
-static int				ft_calcul_size_available(t_metadata **next_bloc, size_t *size_total, int size, t_metadata *bloc);
-static void		*ft_new_allocation(void *ptr, size_t size);
+static void				ft_display_addr(void *ptr);
+static void				ft_print_bloc(t_metadata *bloc);
+static	void			ft_init_first_bloc(t_metadata *first_bloc,
+	t_metadata *new_area);
+int						ft_new_area(t_metadata *start, t_metadata **area,
+	size_t size);
+static void				ft_init_new_area(t_metadata *new_area,
+	size_t size_total, size_t size);
+static void				ft_add_to_last_area(t_metadata *start,
+	t_metadata *new_area);
+static int				ft_add_extra_bloc(void *ptr, size_t size,
+	size_t size_total, t_metadata *bloc);
+static int				ft_calcul_size_available(t_metadata **next_bloc,
+	size_t *size_total, int size, t_metadata *bloc);
+static void				*ft_new_allocation(void *ptr, size_t size);
 
 #endif
